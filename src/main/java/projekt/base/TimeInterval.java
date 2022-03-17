bb759411ddc8ee320314f624a68384e5d3462a99
vasilevaana <62727655+vasilevaana@users.noreply.github.com>
2022-03-17 19:31:41 +0100

    H1.3 fertig

--- src/main/java/projekt/base/TimeInterval.java
+++ src/main/java/projekt/base/TimeInterval.java
@@ -8,6 +8,14 @@ public class TimeInterval {
     private LocalDateTime start;
     private LocalDateTime end;
 
+    /*
+    constructor
+    The public constructor has two parameters of formal type LocalDateTime and uses them to initialize the two attributes. As usual, start with the first and end with the second parameter. If either of the two current parameters is null, a NullPointerException is thrown with the message "start" if the first parameter is null,
+     otherwise with the message "end" if the second parameter is null.
+     If the time window is negative length,throw a IllegalArgumentException
+     @param start
+     @param end
+     */
     public TimeInterval(LocalDateTime start, LocalDateTime end)
     {
         if (start == null)
@@ -23,13 +31,24 @@ public class TimeInterval {
     }
 
 
+    /*
+    @return the value of start
+     */
     public LocalDateTime getStart() {
         return start;
     }
 
+    /*
+    @return the value of end
+     */
     public LocalDateTime getEnd() {
         return end;
     }
+
+    /*
+    calculate the duration from start to end
+    @return the duration
+     */
     public Duration getDuration()
     {
         Duration duration = Duration.between(this.start, this.end);
